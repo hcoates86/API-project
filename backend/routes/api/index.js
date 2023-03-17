75e3183d7d8d39d0727b35c2d555d24666b2eadf
Hcoates <h-coates@live.com>
2023-03-17 12:32:55 -0400

    Add Users table migration and seedsAdd Users table migration and seeds

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,5 +3,18 @@ const router = require('express').Router();
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
-  
+
+  // GET /api/set-token-cookie
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+
 module.exports = router;
