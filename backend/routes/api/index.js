b985bcb4f62e81dff22436d67f4974a3ecd865c4
Hcoates <h-coates@live.com>
2023-03-17 18:52:44 -0400

    working

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,18 +3,23 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.use(restoreUser);
 
 router.use('/users', usersRouter);
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
   // GET /api/set-token-cookie
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
+
 router.get('/set-token-cookie', async (_req, res) => {
   const user = await User.findOne({
     where: {
