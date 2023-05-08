bd4367eba21aea43ab70a88adee16aa9235cc4fa
hcoates86 <hcoates@DESKTOP-IAPKS40.localdomain>
2023-05-08 14:08:05 -0400

    authme done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
-import './index.css';
-import App from './App';
-import configureStore from './store';
-import { restoreCSRF, csrfFetch } from './store/csrf';
+import React from "react";
+
+import "./index.css";
+
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ModalProvider, Modal } from "./context/Modal";
+import App from "./App";
+
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
 
 const store = configureStore();
@@ -17,13 +21,17 @@ if (process.env.NODE_ENV !== "production") {
   window.store = store;
   window.sessionActions = sessionActions;
 }
+
 function Root() {
   return (
-    <ReduxProvider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ReduxProvider>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
   );
 }
 
@@ -31,5 +39,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,70 +1,103 @@
 
 import {csrfFetch} from './csrf';
 
-export const CURRENT_USER = 'session/CURRENT_USER';
-export const REMOVE_USER = 'session/REMOVE_USER';
+const SET_USER = "session/setUser";
+const REMOVE_USER = "session/removeUser";
 
 
-export const sessionUser = (user) => ({
-    type: CURRENT_USER,
-    user
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    payload: user,
+  };
+};
+
+const removeUser = () => {
+  return {
+    type: REMOVE_USER,
+  };
+};
+
+export const login = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch("/api/session", {
+    method: "POST",
+    body: JSON.stringify({
+      credential,
+      password,
+    }),
   });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
 
-  export const removeUser = () => ({
-    type: REMOVE_USER
-  });
-
-
-  export const fetchUser = (user) => async (dispatch) => {
-    const { credential, password } = user;
-    const res = await csrfFetch('/api/session', {
-      method: 'POST',
-      body: JSON.stringify({
-        credential,
-        password
-      })
-    });
   
-    if (res.ok) {
-      const data = await res.json();
-      dispatch(sessionUser(data.user));
-    } return res;
+
+  // export const deleteUser = () => async (dispatch) => {
+  //   const res = await fetch('/api/session', {
+  //     method: 'DELETE',
+  //     headers: {"Content-Type": "application/json"}
+  //   })
+
+  //   if (res.ok) {
+  //     dispatch(removeUser())
+  //   }else {
+  //     const errors = await res.json();
+  //     return errors;
+  // }}
+
+  export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
   };
 
-  export const deleteUser = () => async (dispatch) => {
-    const res = await fetch('/api/session', {
+  export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+  export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
       method: 'DELETE',
-      headers: {"Content-Type": "application/json"}
-    })
-
-    if (res.ok) {
-      dispatch(removeUser())
-    }else {
-      const errors = await res.json();
-      return errors;
-  }
-
-  }
-
-
+    });
+    dispatch(removeUser());
+    return response;
+  };
 
   const initialState = 
   {
     user: null
   }
 
-const sessionReducer = (state = initialState, action) => {
+  const sessionReducer = (state = initialState, action) => {
     let newState;
     switch (action.type) {
-        case CURRENT_USER:
-          newState = Object.assign({}, state);
-          newState.user = action.user;
-          return newState;
-        case REMOVE_USER:
-            return initialState;
-  default:
-    return state
-    
-    }}
+      case SET_USER:
+        newState = Object.assign({}, state);
+        newState.user = action.payload;
+        return newState;
+      case REMOVE_USER:
+        newState = Object.assign({}, state);
+        newState.user = null;
+        return newState;
+      default:
+        return state;
+    }
+  };
 
     export default sessionReducer;
